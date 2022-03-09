605a37177e64fb1ca28a555e61cab47b6f3dbfe3
SAS <100604497+saaiqSAS@users.noreply.github.com>
2022-03-09 17:56:51 +0500

    Add files via upload

---
+++ Wizard/SourceCode/Universal (inteliJ)/SAS-STE-v1-Wizard v1.0.1/SAS-STE-v1-Wizard/src/com/sas/ste/v1/wiz/Main.java
@@ -0,0 +1,26 @@
+package com.sas.ste.v1.wiz;
+
+import java.io.FileNotFoundException;
+
+public class Main {
+
+  public static void main(String[] args) throws FileNotFoundException {
+
+
+System.out.println("#########################################");
+   System.out.println("#     ___   _    ___    ___ ___  __     #");
+   System.out.println("#    /___  /_\\  /___   /___  |  |_      #");
+   System.out.println("#    ___/ /   \\ ___/   ___/  |  |__     #");
+   System.out.println("#       Encryption_Wizard v1.0.1        #");
+   System.out.println("#                                       #");
+   System.out.println("#              Made By SAS              #");
+   System.out.println("#          Saaiq Abdulla Saeed          #");
+System.out.println("#########################################");
+System.out.println("\n");
+   
+   encryptionProgram.encryptionProgram();
+
+   // < not decrypting bug fixed, changed 55 to change to 92 instead of 93 in L2p1
+   // test souts in keys fixed
+  }
+}
